aa7220ae8a9729a635420bd7388728c3c66db27c
diala <dialachamandi@gmail.com>
2017-10-23 21:31:13 +0400

    add a javascript file

---
+++ javascript.js
@@ -0,0 +1,110 @@
+
+let classes =["diala","tamim","wajdi","hanan"];
+
+console.log(classes[0],classes[1])
+
+
+let car=[ {
+  type:"mercedes",
+  year:2017,
+  color:"dark grey"
+
+},
+{
+  type:"rangler robicon",
+  year:2017,
+  color:"black",
+}
+
+];
+console.log(car[1].type);
+
+function isequal(number1, number2)
+{
+  if(number1>=number2)
+  {
+    console.log("they are equal");
+  }
+
+  else
+  {
+    console.log( "they are not equal");
+  }
+
+
+
+
+
+
+}
+
+const x=25;
+const y=26;
+
+isequal(x,y);
+
+const students=[
+  { name:"stephany",
+  age:21
+
+},
+{
+  name:"alaa",
+  age:24
+},
+{
+  name:"cynthia",
+  age:21
+}
+];
+
+for(let i=0;i<3;i++)
+{
+
+  console.log(students[i].name);
+
+}
+
+let j=0
+
+while(j<3)
+{
+  console.log(students[j].name);
+  j++;
+}
+
+
+
+let array= ["d","i","a","l","a"];
+
+array.pop("a")
+array.push("x")
+console.log(array);
+
+let name = "diala";
+name=name.split();
+console.log(name);
+
+const numbers =[1,4,9];
+
+let doubles = numbers.map(function(num) {
+  return num *2;
+});
+
+function myfunction(myarray)
+{
+
+  if(1<2),(2<3)
+  
+  console.log([1] "is the smallest value")
+
+  if(3>2),(3>1)
+
+    console.log([3] "is the largest value")
+
+
+}
+
+
+let isnumbers =[1,2,3,};
+  myfunction(isnumbers);
